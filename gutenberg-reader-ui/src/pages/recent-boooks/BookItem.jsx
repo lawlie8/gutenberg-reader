444ce127a705898871b67fbacf2c8c7ef26b654f
lawlie8 <lawlie8.pm.me>
2024-08-11 22:01:35 +0530

    Book Item Updated with download icons and urls

--- gutenberg-reader-ui/src/pages/recent-boooks/BookItem.jsx
+++ gutenberg-reader-ui/src/pages/recent-boooks/BookItem.jsx
@@ -1,9 +1,13 @@
-import { Card, Col, Layout, Row } from 'antd';
+import { Avatar, Card, Col, Layout, List, Row } from 'antd';
 import './recent-books.css'
 import { Content, Footer } from 'antd/es/layout/layout';
 import { Image } from 'antd';
+import { MoreOutlined,ReadOutlined,FileZipOutlined,LinkOutlined, BookOutlined } from '@ant-design/icons';
 import Meta from 'antd/es/card/Meta';
+import React from 'react';
 export default function BookItem({ title, link, description }) {
+
+    const [downloadWindowFlag,setDownLoadWindowFlag] = React.useState(false);
 
     function parseImageData(link) {
         var bookId = link?.split("/").pop().split("\"")[0]
@@ -18,7 +22,7 @@ export default function BookItem({ title, link, description }) {
 
     function parseDescription(title){
         let Author = title?.split("by ")[1];
-        if(Author == undefined){
+        if(Author === undefined){
             return "Un-known"
         }else if(Author.length > 20){
             return Author?.substring(0,20)
@@ -26,13 +30,61 @@ export default function BookItem({ title, link, description }) {
         return Author?.substring(0,Author.length - 1)
     }
 
-    function navigateToBookUrl(link){
+
+
+    function openDownloadOptionPop(link){
+        
+        setDownLoadWindowFlag(!downloadWindowFlag)
+    }
+
+
+    var checkNavigation = ()=>{
+        if(!downloadWindowFlag){
+            return "none";
+        }else{
+            return "block";
+        }
+    }
+
+
+    function manageReadLocal(link){
+        console.log("Reading Locally");
+        
+    }
+
+    function manageReadPlainHtml(link){
+        console.log("Reading Plain HTML Only");
+        
+    }
+
+    function manageFileZipDownload(link){
+        var bookId = link?.split("/").pop().split("\"")[0]
+        var index = link?.split(".org")[0] + ".org";
+        window.location = index.substring(1, index.length) + `/cache/epub/` + bookId + `/pg${bookId}` + `-h.zip`;
+    }
+
+    function manageLinkOutSource(link){
         link = link.replaceAll("\"","");
         window.location = link;
     }
 
+//onClick={() => navigateToBookUrl(link)}
     return (
-        <div className="book-item" onClick={() => navigateToBookUrl(link)}>
+        <div className="book-item" >
+            <div className="download-book-icon"  >
+                <MoreOutlined onClick={()=> openDownloadOptionPop(link)} style={{fontSize: '18px',color:'black'}}/>
+                <div className="download-book-item-option-list" style={{display:checkNavigation()}}>
+                    <List>
+                        <List.Item><ReadOutlined onClick={()=> manageReadLocal(link)} style={{fontSize: '20px',color:'white',paddingLeft:'2px'}} /></List.Item>
+                        <List.Item><BookOutlined onClick={()=> manageReadPlainHtml(link)} style={{fontSize: '20px',color:'white',paddingLeft:'2px'}} /></List.Item>
+                        <List.Item><FileZipOutlined onClick={()=> manageFileZipDownload(link)} style={{fontSize: '20px',color:'white',paddingLeft:'2px'}} /></List.Item>
+                        <List.Item><LinkOutlined onClick={()=> manageLinkOutSource(link)}  style={{fontSize: '20px',color:'white',paddingLeft:'2px'}} /></List.Item>
+                    </List>
+                </div>
+            </div>
+            
+  
+
             <Card
                 hoverable
                 style={{ width: 240 }}
@@ -41,5 +93,6 @@ export default function BookItem({ title, link, description }) {
                 <Meta title={parseTitle(title)} description={parseDescription(title)}/>
             </Card>
         </div>
+        
     )
 }
