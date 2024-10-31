fa4c67921f10b85e5a84f9920ea792c0ac02faf0
lawlie8 <lawlie8.pm.me>
2024-10-31 08:32:19 +0530

    Fixed Issues with un-authorized access to pages/redirect

---
+++ gutenberg-reader-ui/src/utils/axios.js
@@ -0,0 +1,53 @@
+import axios from "axios";
+import { BASE_URL } from "../constants";
+import { notification } from "antd";
+
+function fetchBaseUrl(){
+    console.log('Checking url');
+    axios.get("https://www.redirectcheck.org/api/check?url=http%3A%2F%2Freader.lawlie8.org").then((response) =>{
+        return response.data.data.final_result.final_url
+    }).catch((error)=> {
+        console.log(error);
+    })
+}
+
+
+const instance = axios.create({
+    baseURL: fetchBaseUrl()
+})
+
+instance.interceptors.response.use(
+    response => {
+        if(response.status === 302 || response.status === 403 || response.status === 404){
+            notification.error({
+                message:"Error",
+                duration:1,
+                description:"Something Went Wrong",
+                style: { width: '250px' }
+            })
+        window.location.href = "/"
+        }
+
+    return response
+    },error =>{
+        if(error.response.status === 403){
+            notification.error({
+            message:error.response.code,
+            duration:1,
+            description:"Login Required",
+            style: { width: '250px' }
+        })
+        const delayFucnt = setTimeout(() => {
+            window.location.href = "/"
+        }, 1000);
+
+    }
+
+    return error
+
+    }
+)
+
+
+
+export default instance;
