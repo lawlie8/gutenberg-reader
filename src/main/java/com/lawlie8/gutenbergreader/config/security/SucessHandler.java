e0e1fdf67f6118757c35c19a2baffef850342974
lawlie8 <lawlie8.pm.me>
2024-11-04 23:12:43 +0530

    Fixed issue with login page autofill bgColor

--- src/main/java/com/lawlie8/gutenbergreader/config/security/SucessHandler.java
+++ src/main/java/com/lawlie8/gutenbergreader/config/security/SucessHandler.java
@@ -3,11 +3,10 @@ package com.lawlie8.gutenbergreader.config.security;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
 
 import java.io.IOException;
 
